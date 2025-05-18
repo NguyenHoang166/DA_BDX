465e7dae9c2351fa3e891740099c851e90ca1791
Nguyễn Hoàng <nguyenhoangdn56@gmail.com>
2025-05-18 07:40:38 +0700

    cập nhật

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/admin'); 
+const userRoutes = require('./routes/users'); 
 const feedbackRoutes = require('./routes/feedback');
 const payRoutes = require('./routes/pay');
 const statisticalRouter = require('./routes/statistical');
@@ -15,13 +15,13 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
+app.use('/api/user', userRoutes); // Sửa từ usersRoutes thành userRoutes
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/thanh-toan', payRoutes);
 app.use('/api/statistics', statisticalRouter);
+
 // Khởi động server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
 });
-
